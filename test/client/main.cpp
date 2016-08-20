0ed29be7c6d75dc25bf8c2ffe500e46cc2c2d186
chxuan <787280310@qq.com>
2016-08-20 18:16:33 +0800

    Update test

--- test/client/main.cpp
+++ test/client/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <thread>
 #include "easyrpc/EasyRpc.hpp"
+#include "easyrpc/client/Protocol.hpp"
+using namespace easyrpc;
 
 EASYRPC_RPC_PROTOCOL_DEFINE(add, int(int, int));
 
@@ -11,6 +13,8 @@ int main()
     {
         client.run();
         client.connect("127.0.0.1", 8888);
+        auto ret = client.call(add, 1, 2);
+        std::cout << "ret: " << ret << std::endl;
     }
     catch (std::exception& e)
     {
